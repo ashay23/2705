265a83a061898fe760869b0005d239f7c0c55222
ashay23 <141316797+ashay23@users.noreply.github.com>
2024-05-27 08:03:15 +0530

    Add files via upload

---
+++ AllTimeHigh.java
@@ -0,0 +1,70 @@
+
+
+import java.io.*;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+public class AllTimeHigh {
+	
+	public static class MapClass extends Mapper<LongWritable,Text,Text,DoubleWritable>
+	   {
+	      public void map(LongWritable key, Text value, Context context)
+	      {	    	  
+	         try{
+	            String[] str = value.toString().split(",");	 
+	            double high_price = Double.parseDouble(str[4]);
+	            context.write(new Text(str[1]),new DoubleWritable(high_price));
+	         }
+	         catch(Exception e)
+	         {
+	            System.out.println(e.getMessage());
+	         }
+	      }
+	   }
+	
+	  public static class ReduceClass extends Reducer<Text,DoubleWritable,Text,DoubleWritable>
+	   {
+		    private DoubleWritable result = new DoubleWritable();
+		    
+		    public void reduce(Text key, Iterable<DoubleWritable> values,Context context) throws IOException, InterruptedException {
+		      double max_price = 0;
+				
+		         for (DoubleWritable val : values)
+		         {       	
+		        	if (val.get() > max_price)
+		        	{
+		        		max_price = val.get();
+		        	}
+		         }
+		         
+		      result.set(max_price);		      
+		      context.write(key, result);
+		      //context.write(key, new DoubleWritable(sum));
+		      
+		    }
+	   }
+	  public static void main(String[] args) throws Exception {
+		    Configuration conf = new Configuration();
+		    //conf.set("name", "value")
+		    //conf.set("mapreduce.input.fileinputformat.split.minsize", "134217728");
+		    Job job = Job.getInstance(conf, "All time high price for each stock");
+		    job.setJarByClass(AllTimeHigh.class);
+		    job.setMapperClass(MapClass.class);
+		    //job.setCombinerClass(ReduceClass.class);
+		    job.setReducerClass(ReduceClass.class);
+		    job.setNumReduceTasks(1);
+		    job.setOutputKeyClass(Text.class);
+		    job.setOutputValueClass(DoubleWritable.class);
+		    FileInputFormat.addInputPath(job, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		  }
+}
---
+++ AllTimeLow.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+public class AllTimeLow {
+	
+	public static class MapClass extends Mapper<LongWritable,Text,Text,DoubleWritable>
+	   {
+	      public void map(LongWritable key, Text value, Context context)
+	      {	    	  
+	         try{
+	            String[] str = value.toString().split(",");	 
+	            double min_price = Double.parseDouble(str[5]);
+	            context.write(new Text(str[1]),new DoubleWritable(min_price));
+	         }
+	         catch(Exception e)
+	         {
+	            System.out.println(e.getMessage());
+	         }
+	      }
+	   }
+	
+	  public static class ReduceClass extends Reducer<Text,DoubleWritable,Text,DoubleWritable>
+	   {
+		    private DoubleWritable result = new DoubleWritable();
+		    
+		    public void reduce(Text key, Iterable<DoubleWritable> values,Context context) throws IOException, InterruptedException {
+		      double min_price = Double.POSITIVE_INFINITY;
+				
+		         for (DoubleWritable val : values)
+		         {       	
+		        	if (val.get() < min_price)
+		        	{
+		        		min_price = val.get();
+		        	}
+		         }
+		         
+		      result.set(min_price);		      
+		      context.write(key, result);
+		      //context.write(key, new DoubleWritable(sum));
+		      
+		    }
+	   }
+	  public static void main(String[] args) throws Exception {
+		    Configuration conf = new Configuration();
+		    //conf.set("name", "value")
+		    //conf.set("mapreduce.input.fileinputformat.split.minsize", "134217728");
+		    Job job = Job.getInstance(conf, "All time low price for each stock");
+		    job.setJarByClass(AllTimeLow.class);
+		    job.setMapperClass(MapClass.class);
+		    //job.setCombinerClass(ReduceClass.class);
+		    job.setReducerClass(ReduceClass.class);
+		    job.setNumReduceTasks(1);
+		    job.setOutputKeyClass(Text.class);
+		    job.setOutputValueClass(DoubleWritable.class);
+		    FileInputFormat.addInputPath(job, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		  }
+}
